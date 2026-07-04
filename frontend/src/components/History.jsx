c4aab2a2e26c1a357234a32839dbaaa28abe19ac
Ankit Kumar <ritickthakur5@gmail.com>
2026-07-04 19:13:24 +0530

    Moved api.js into src

--- frontend/src/components/History.jsx
+++ frontend/src/components/History.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import API from "../../api";
+import API from "../api";
 function History() {
 
     const [history, setHistory] = useState([]);
